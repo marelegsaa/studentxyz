448c3bf63ffadbd6f6b4e7d3be85b9b1728aab56
marelegsaa <atatsigamerul@gmail.com>
2025-08-16 17:07:29 +0300

    credits added for subjects

--- validari.py
+++ validari.py
@@ -62,7 +62,8 @@ MATERII = {
         '3-2': [
             'sociologie', 'serii de timp', 'rețele de calculatoare',
             'pachete software', 'sisteme informaționale economice',
-            'calitate și testare software'
+            'calitate și testare software', 'dreptul afacerilor', 
+            'drept', 'pregătire lucrare licență'
         ]
     },
     'statistică și previziune economică': {
@@ -121,4 +122,245 @@ OPTIUNI_OPTIONALE = {
         'dezvoltare durabilă', 'analiza economico-financiară',
         'tehnici de scriere academică și învățare eficientă'
     ]
+}
+
+CREDITE = {
+    'cibernetică economică': {
+        '1-1': {
+            'economie': 5,
+            'algebră': 5,
+            'bazele statisticii': 5,
+            'bazele cercetărilor operaționale': 4,
+            'bazele tehnologiei informației': 6,
+            'bazele programării calculatoarelor': 3,
+            'limba engleză și comunicare de specialitate 1': 2,
+            'educație fizică și sport': 1
+        },
+        '1-2': {
+            'analiză matematică': 5,
+            'statistică': 5,
+            'algoritmi și tehnici de programare': 4,
+            'sisteme de operare': 4,
+            'bazele ciberneticii economice': 4,
+            'marketing': 4,
+            'contabilitate': 4,
+            'educație fizică și sport': 1
+        },
+        '2-1': {
+            'programare orientată obiect': 5,
+            'baze de date': 5,
+            'statistică macroeconomică': 4,
+            'probabilități și statistică matematică': 4,
+            'microeconomie cantitativă': 4,
+            'management': 4,
+            'finanțe': 4,
+            'educație fizică și sport': 1
+        },
+        '2-2': {
+            'analiza și diagnoza sistemelor economice': 4,
+            'macroeconomie cantitativă': 4,
+            'teoria deciziei': 4,
+            'simularea proceselor economice': 4,
+            'econometrie': 4,
+            'microeconomie managerială': 4,
+            'practică de specialitate': 3
+        },
+        '3-1': {
+            'cibernetica sistemelor economice': 5,
+            'cercetări operaționale': 5,
+            'economia și gestiunea riscului': 4,
+            'sisteme suport de decizie': 4,
+            'analiza datelor': 4,
+            'dinamica sistemelor economice': 4,
+            'sondaje și anchete statistice': 4,
+            'demografie': 4,
+            'tehnologii web': 4,
+            'multimedia': 4,
+            'sgbd oracle': 4,
+            'robotică': 4,
+            'modelare stochastică în domeniul economic': 4
+        },
+        '3-2': {
+            'sociologie': 2,
+            'serii de timp': 4,
+            'sisteme informaționale pentru conducere': 5,
+            'pachete software': 5,
+            'teoria jocurilor': 5,
+            'inteligență computațională în economie': 5,
+            'dreptul afacerilor': 4,
+            'drept': 4,
+            'pregătire lucrare licență': 0
+        }
+    },
+    'informatică economică': {
+        '1-1': {
+            'economie': 5,
+            'algebră': 5,
+            'bazele statisticii': 5,
+            'bazele cercetărilor operaționale': 4,
+            'bazele tehnologiei informației': 6,
+            'bazele programării calculatoarelor': 3,
+            'limba engleză și comunicare de specialitate 1': 2,
+            'educație fizică și sport': 1
+        },
+        '1-2': {
+            'analiză matematică': 5,
+            'statistică': 5,
+            'algoritmi și tehnici de programare': 4,
+            'sisteme de operare': 4,
+            'bazele ciberneticii economice': 4,
+            'marketing': 4,
+            'contabilitate': 4,
+            'educație fizică și sport': 1
+        },
+        '2-1': {
+            'programare orientată obiect': 5,
+            'baze de date': 5,
+            'statistică macroeconomică': 4,
+            'probabilități și statistică matematică': 4,
+            'microeconomie cantitativă': 4,
+            'management': 4,
+            'finanțe': 4,
+            'educație fizică și sport': 1
+        },
+        '2-2': {
+            'programarea aplicațiilor windows': 4,
+            'macroeconomie cantitativă': 4,
+            'programare evolutivă și algoritmi genetici': 4,
+            'structuri de date': 4,
+            'programare multiparadigmă - java': 4,
+            'sgbd oracle': 4,
+            'practică de specialitate': 3
+        },
+        '3-1': {
+            'econometrie': 4,
+            'analiza și proiectarea sistemelor informatice': 5,
+            'dispozitive și aplicații mobile': 5,
+            'multimedia': 4,
+            'dezvoltare software pentru analiza datelor': 4,
+            'tehnologii web': 4,
+            'demografie': 4,
+            'sondaje și anchete statistice': 4,
+            'robotică': 4,
+            'modelare stochastică în domeniul economic': 4
+        },
+        '3-2': {
+            'sociologie': 2,
+            'serii de timp': 4,
+            'rețele de calculatoare': 5,
+            'pachete software': 5,
+            'sisteme informaționale economice': 5,
+            'calitate și testare software': 5,
+            'dreptul afacerilor': 4,
+            'drept': 4,
+            'pregătire lucrare licență': 0
+        }
+    },
+    'statistică și previziune economică': {
+        '1-1': {
+            'economie': 5,
+            'algebră': 5,
+            'bazele statisticii': 5,
+            'bazele cercetărilor operaționale': 4,
+            'bazele tehnologiei informației': 6,
+            'bazele programării calculatoarelor': 3,
+            'limba engleză și comunicare de specialitate 1': 2,
+            'educație fizică și sport': 1
+        },
+        '1-2': {
+            'analiză matematică': 5,
+            'statistică': 5,
+            'algoritmi și tehnici de programare': 4,
+            'sisteme de operare': 4,
+            'bazele ciberneticii economice': 4,
+            'marketing': 4,
+            'contabilitate': 4,
+            'educație fizică și sport': 1
+        },
+        '2-1': {
+            'programare orientată obiect': 5,
+            'baze de date': 5,
+            'statistică macroeconomică': 4,
+            'probabilități și statistică matematică': 4,
+            'microeconomie cantitativă': 4,
+            'management': 4,
+            'finanțe': 4,
+            'educație fizică și sport': 1
+        },
+        '2-2': {
+            'pachete software': 4,
+            'statistică neparametrică': 4,
+            'econometrie': 4,
+            'statistică spațială': 4,
+            'testarea ipotezelor statistice': 4,
+            'macroeconomie cantitativă': 4,
+            'practică de specialitate': 3
+        },
+        '3-1': {
+            'modelarea și vizualizarea geospațială a datelor statistice': 4,
+            'controlul statistic al calității': 5,
+            'analiză statistică multidimensională': 5,
+            'sondaje și anchete statistice': 4,
+            'demografie': 4,
+            'teoria jocurilor': 4,
+            'cercetări operaționale': 4,
+            'dinamica sistemelor economice': 4,
+            'programarea aplicațiilor windows': 4,
+            'tehnologii web': 4,
+            'sgbd oracle': 4,
+            'robotică': 4,
+            'modelare stochastică în domeniul economic': 4
+        },
+        '3-2': {
+            'previziune economică': 5,
+            'statistica piețelor financiare': 5,
+            'serii de timp': 5,
+            'statistică microeconomică': 4,
+            'sociologie': 2,
+            'proiectarea sistemelor informatice în statistică': 5,
+            'dreptul afacerilor': 3,
+            'drept': 3,
+            'pregătire lucrare licență': 0
+        }
+    }
+}
+
+CREDITE_OPTIONALE = {
+    '2-1': {
+        'construcție şi depanare pc': 4,
+        'modele regionale de economie': 4,
+        'istoria economiei': 4,
+        'comunicare în limba engleză 1': 4,
+        'comunicare în limba franceză 1': 4,
+        'comunicare în limba rusă 1': 4,
+        'comunicare în limba spaniolă 1': 4,
+        'comunicare în limba italiană 1': 4,
+        'comunicare în limba germană 1': 4,
+        'comunicare în limba turcă 1': 4,
+        'comunicare în limba japoneză 1': 4,
+        'comunicare în limba chineză 1': 4
+    },
+    '2-2': {
+        'doctrine economice': 4,
+        'competiție și competitivitate': 4,
+        'comunicare în limba engleză 2': 4,
+        'comunicare în limba germană 2': 4,
+        'comunicare în limba turcă 2': 4,
+        'comunicare în limba japoneză 2': 4,
+        'comunicare în limba chineză 2': 4,
+        'comunicare în limba franceză 2': 4,
+        'comunicare în limba rusă 2': 4,
+        'comunicare în limba spaniolă 2': 4,
+        'comunicare în limba italiană 2': 4,
+        'managementul riscului în afaceri internaționale': 3,
+        'antreprenoriat în comerț, turism și servicii': 3,
+        'finanțe corporative': 3,
+        'negocieri internaționale': 3,
+        'managementul relațiilor cu clienții': 3,
+        'etică şi integritate academică': 3,
+        'economia informației digitale cu aplicații în afaceri': 3,
+        'dezvoltare durabilă': 3,
+        'analiza economico-financiară': 3,
+        'tehnici de scriere academică și învățare eficientă': 3,
+    }
 }
