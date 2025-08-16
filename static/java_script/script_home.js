6cd3e9686407f7bcc1ebac180b86fd6ecd282ab9
marelegsaa <atatsigamerul@gmail.com>
2025-08-16 17:12:15 +0300

    fixed mobile nav

--- static/java_script/script_home.js
+++ static/java_script/script_home.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
   var nav = document.querySelector('.nav');
   var navExpand = document.querySelector('.nav_expand');
-  var navListItem = document.querySelectorAll('.nav_listitem');
 
-  navExpand.addEventListener('click', function() {
-    nav.classList.toggle('nav_closed');
-  });
+  if (window.innerWidth <= 768) {
+    nav.classList.remove('nav_closed');
+  } else {
+    navExpand.addEventListener('click', function() {
+      nav.classList.toggle('nav_closed');
+    });
+  }
 });
